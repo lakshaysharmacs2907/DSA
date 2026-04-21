43ff1a3272d1aa63e7e84c35f5725b0019f9fb66
lakshaysharmacs2907 <lakshay.sharma.cs27@iilm.edu>
2026-04-21 10:11:20 +0530

    Create Path Sum II.java

---
+++ Path Sum II.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        
+        dfs(root, targetSum, path, result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, int sum, List<Integer> path, List<List<Integer>> result) {
+        if (node == null) return;
+
+        path.add(node.val);
+
+        // Leaf node
+        if (node.left == null && node.right == null && sum == node.val) {
+            result.add(new ArrayList<>(path)); // copy path
+        } else {
+            dfs(node.left, sum - node.val, path, result);
+            dfs(node.right, sum - node.val, path, result);
+        }
+
+        // Backtrack
+        path.remove(path.size() - 1);
+    }
+}
